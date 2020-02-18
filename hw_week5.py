66c9e15417c9e72bb6f7d415bd05ceaf0576c3b4
Siwel <zuytrew951753@gmail.com>
2020-02-18 16:58:42 +0800

    Update 9*9

--- hw_week5.py
+++ hw_week5.py
@@ -1,7 +1,7 @@
 def hw_week5():
-    # Your code here
-    pass
-
+    for i in range(1, 10, 1):
+        for j in range(1, 10, 1):
+            print(str(i) + 'X' + str(j) + '=' + str(i*j))
 
 if __name__ == '__main__':
     hw_week5()
