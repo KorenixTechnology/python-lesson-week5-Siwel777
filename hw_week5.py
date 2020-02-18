9e7bb1cdd8ab9178c1bb80b9d57a2f2ac6a364ce
Siwel <zuytrew951753@gmail.com>
2020-02-18 17:06:41 +0800

    X to x

--- hw_week5.py
+++ hw_week5.py
@@ -1,7 +1,7 @@
 def hw_week5():
     for x in range(1, 10, 1):
         for y in range(1, 10, 1):
-            print(str(x) + 'X' + str(y) + '=' + str(x*y))
+            print(str(x) + 'x' + str(y) + '=' + str(x*y))
 
 if __name__ == '__main__':
     hw_week5()
